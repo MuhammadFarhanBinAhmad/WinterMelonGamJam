211ad7ff93e9f514a61caa9445b24f80e5cd3d6d
CheekyKnob <44768125+MuhammadFarhanBinAhmad@users.noreply.github.com>
2024-12-30 03:05:07 +0800

    MainMenuAnim

--- WinterMelon/Assets/Script/Menu/Buttons/MenuButton.cs
+++ WinterMelon/Assets/Script/Menu/Buttons/MenuButton.cs
@@ -5,43 +5,36 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class MenuButton : MonoBehaviour
 {
-    public Image b_Arrow;
-
-    public RectTransform rt_Ship;
-    RectTransform rt_Button;
 
     public AudioSource _AudioSound;
     public AudioClip m_MouseOnEnterSound;
 
-    public GameObject m_Projectile;
-
-    private void Start()
-    {
-        rt_Button = GetComponent<RectTransform>();
-    }
+    public Animator _Animator;
 
     public void WhenMouseEnter()
     {
-        b_Arrow.transform.position = new Vector3(b_Arrow.transform.position.x,
-                                                transform.position.y,
-                                                transform.position.z);
-
-        Vector3 direction = rt_Button.position - rt_Ship.position;
-
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-        rt_Ship.rotation = Quaternion.Euler(0, 0, angle);
-
         _AudioSound.clip = m_MouseOnEnterSound;
         _AudioSound.Play();
     }
-    public void SetAndShootProjectile()
+    public void StartGame()
     {
-        GameObject go = Instantiate(m_Projectile, rt_Ship.position, rt_Ship.rotation);
-        go.SetActive(false);
-        go.transform.SetParent(GameObject.Find("Canvas").transform);
-        go.GetComponent<MenuProjectile>().targetImage = rt_Button;
-        go.SetActive(true);
+        _Animator.SetTrigger("StartGame");
+    }
+    public void MainMenuToOption()
+    {
+        _Animator.SetTrigger("MainMenuToOption");
+    }
+    public void OptionToMainMenu()
+    {
+        _Animator.SetTrigger("OptionToMainMenu");
+    }
+    public void MainMenuToCredit()
+    {
+        _Animator.SetTrigger("MainMenuToCredits");
+    }
+    public void CreditToMainMenu()
+    {
+        _Animator.SetTrigger("CreditsToMainMenu");
     }
     public void ChangeScene(string scene)
     {
