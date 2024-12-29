2e2713418ded4b033afeee69d7848cb838390967
Hizzykid <112919588+B0B0ChaCha@users.noreply.github.com>
2024-12-30 02:26:39 +0800

    Made changes on what Farhan tell me to do for UI
    Update money when you buy
    Remove money text from shop
    Add time system
    Show current time with text that has been provided
    When you enter the drop point, have text pop up saying "Press 'E' to open store"

--- WinterMelon/Assets/Script/Player/ShipStatsManager.cs
+++ WinterMelon/Assets/Script/Player/ShipStatsManager.cs
@@ -4,9 +4,48 @@ using UnityEngine;
 
 public class ShipStatsManager : MonoBehaviour
 {
-    public int level_Radius,level_PolarityForce,
-               level_ThrustForce,level_ShipMaxSpeed;
+    [Header("Stats")]
+    public int level_Radius;
+    public int level_PolarityForce;
+    public int level_ThrustForce;
+    public int level_ShipMaxSpeed;
 
+    [Header("Money")]
     public int _TotalMoney;
 
+    [Header("Timer")]
+    //public float _Time = 60.0f;
+    public int _Min = 1;
+    public int _Sec = 30;
+
+    [HideInInspector]
+    public float _currentTime = 0.0f;
+    [HideInInspector]
+    public bool isTimerRunning = false; //might pause for stores?
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _currentTime = (_Min * 60) + _Sec; //min * 60s + number of secs
+        isTimerRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimerCountdown();
+    }
+
+    private void TimerCountdown()
+    {
+        if (isTimerRunning)
+        {
+            _currentTime -= Time.deltaTime;
+            if (_currentTime <= 0.0f)
+            {
+                isTimerRunning = false;
+                _currentTime = 0.0f;
+            }
+        }
+    }
 }
