0d8efc154d643c7c3f8573b0f0e64900ba4bce8f
JarrettAng <jarrett_ang@hotmail.com>
2024-12-30 22:29:20 +0800

    Added ship

--- WinterMelon/Assets/Script/Player/ShipCollectRadius.cs
+++ WinterMelon/Assets/Script/Player/ShipCollectRadius.cs
@@ -19,12 +19,22 @@ public class ShipCollectRadius : MonoBehaviour
     public CircleCollider2D ship_PolarityCollider;
 
     public Transform m_Circle;
+    private SpriteRenderer m_CircleSprite;
+    public Color m_CircleAttract;
+    public Color m_CircleRepel;
+
     public GameObject ship_Attract;
     public GameObject ship_Repel;
+
+    public SpriteRenderer m_ship;
+    public Sprite m_shipAttract;
+    public Sprite m_shipRepel;
 
     private void Start()
     {
         ship_PolarityCollider = GetComponent<CircleCollider2D>();
+
+        m_CircleSprite = m_Circle.GetComponent<SpriteRenderer>();
     }
 
     public void Update()
@@ -37,11 +47,15 @@ public class ShipCollectRadius : MonoBehaviour
                 ship_Polarity = POLARITY.ATTRACT;
                 ship_Attract.SetActive(true);
                 ship_Repel.SetActive(false);
+                m_ship.sprite = m_shipAttract;
+                m_CircleSprite.color = m_CircleAttract;
             }
             else
             {
                 ship_Attract.SetActive(false);
                 ship_Repel.SetActive(true);
+                m_ship.sprite = m_shipRepel;
+                m_CircleSprite.color = m_CircleRepel;
             }
         }
     }
