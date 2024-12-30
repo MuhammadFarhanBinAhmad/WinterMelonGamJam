321db462ca234c4dfdcd2ad1f1dee7a2283ff0bd
Hizzykid <112919588+B0B0ChaCha@users.noreply.github.com>
2024-12-30 23:02:48 +0800

    SpeedBar Update
    Increase Size upon purchase
    Changing of colour

--- WinterMelon/Assets/Script/UI/ShipUIManager.cs
+++ WinterMelon/Assets/Script/UI/ShipUIManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class ShipUIManager : MonoBehaviour
 {
@@ -8,11 +9,26 @@ public class ShipUIManager : MonoBehaviour
     ShipMovementControl _shipMovementControl;
     ShipStatsManager _shipStatsManager;
 
+    public Image ship_SpeedbarFill;
+    public RectTransform ship_SpeedbarBaseRect;
+    public RectTransform ship_SpeedbarRect;
+    private float minBarHeight = 250.0f;
+    private float maxBarHeight = 500.0f;
+    [Header("Gradient Colors")]
+    public Color lowSpeedColor = Color.green; // Color at minimum speed
+    public Color highSpeedColor = Color.red; // Color at maximum speed
+
+    public float currentSpeed = 0.0f;
+    public float maxSpeed = 100.0f;
 
     private void Start()
     {
         _shipMovementControl = FindAnyObjectByType<ShipMovementControl>();
         _shipStatsManager = FindAnyObjectByType<ShipStatsManager>();
+        ship_SpeedbarFill = GameObject.Find("SpeedBar").GetComponent<Image>();
+        ship_SpeedbarFill.fillAmount = 0.0f;
+        ship_SpeedbarBaseRect = GameObject.Find("SpeedBarBase").GetComponent<RectTransform>();
+        ship_SpeedbarRect = GameObject.Find("SpeedBar").GetComponent<RectTransform>();
 
         //_countdownTimer.isRunning = true;
     }
@@ -21,8 +37,12 @@ public class ShipUIManager : MonoBehaviour
     {
         //ship_TimeLeft.text = "Time Left: ";
         ship_CurrentSpeed.text = "Speed: " + _shipMovementControl._rb.linearVelocity.magnitude.ToString("F0") + '/' + _shipMovementControl.ship_MaxSpeed;
+       
+        //Update whatever the ship need
         UpdateTimer();
         UpdateMoneyUI();
+        UpdateMaxSpeed();
+        UpdateSpeedBar();
     }
 
     public void UpdateTimer()
@@ -37,8 +57,29 @@ public class ShipUIManager : MonoBehaviour
         ship_TotalMoney.text = "$" + _shipStatsManager._TotalMoney;
     }
 
-    public void ShowUIOpenStore()
+    public void UpdateSpeedBar()
     {
-        
+        currentSpeed = _shipMovementControl._rb.linearVelocity.magnitude;
+        maxSpeed = _shipMovementControl.ship_MaxSpeed;
+
+        currentSpeed = Mathf.Clamp(currentSpeed, 0, maxSpeed);
+        float fillBar = currentSpeed / maxSpeed;
+        ship_SpeedbarFill.fillAmount = fillBar;
+        ship_SpeedbarFill.color = Color.Lerp(lowSpeedColor, highSpeedColor, fillBar);
+        // For checking
+        //Debug.Log("Fill Bar:" + ship_SpeedbarFill.fillAmount);
+        //Debug.Log("Curr Speed:" + currentSpeed);
+        //Debug.Log("Max Speed:" + maxSpeed);
+    }
+
+    public void UpdateMaxSpeed() //This function helps to update max speed as it constantly changing when player purchases upgrades
+    {
+        maxSpeed = _shipMovementControl.ship_MaxSpeed;
+
+		float newHeight = Mathf.Clamp(maxSpeed * 15, minBarHeight, maxBarHeight); //technically width because i rotate
+		float currWidth = 100.0f;
+        ship_SpeedbarBaseRect.sizeDelta = new Vector2(newHeight, currWidth);
+        ship_SpeedbarRect.sizeDelta = new Vector2(newHeight, currWidth);
+
     }
 }
